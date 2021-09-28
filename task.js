f01ad3526e04f842666a8cc00c9d378236dafbf7
Noaman Monther <83986256+devnoaman@users.noreply.github.com>
2021-09-28 20:49:47 +0300

    Task Done Successfully , Please checkout comments

--- task.js
+++ task.js
@@ -1,4 +1,19 @@
-let numbers = [
+
+// * Noaman Monther Mahmood
+// * Full-Stack Development Bootcamp  (Tasks - 1)
+// * JS Side 
+// * Use Better Comments {for better experiance with this task solution}
+// * https://marketplace.visualstudio.com/items?itemName=aaron-bond.better-comments
+
+ 
+
+
+
+
+var bs = require('./helper')
+var fileGeneator = require('./generateFile')
+// * Solving for TODO 3 by macking this list immutable
+const numbers = [
     5,
     8,
     0,
@@ -8,40 +23,21 @@ let numbers = [
     15,
     16
 ];
+// * take a mutable copy of numbers array and dealing with it
+let mutableNumbers = numbers
+// * Solving for TODO 2 ,the code now is well-designed and well-styled
+console.log("Original numbers list: ")
+console.log(mutableNumbers)
+console.log('-----------------------------------')
+console.log("numbers list After Desc sorting: ")
+// * Solving for TODO 1 , after figuring the issu in sorting algorithim and 
+// * and applying TODO 2 for well-designed and well-styled code 
+bs.bubbleSort(mutableNumbers, true)
+console.log('-----------------------------------')
+console.log("numbers listAfter ASDC sorting: ")
+// ! reading a file and sort its elements in ASC order 
 
-console.log("Original numbers list: ",numbers)
-
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        
-        if(numbers[i] > numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
-}
-
-console.log("numbers list After sorting: ",numbers)
-
-
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        
-        if(numbers[i] < numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
-}
-
-console.log("numbers list After Desc sorting: ",numbers)
-
+bs.bubbleSort(mutableNumbers,false)
+fileGeneator.dealingWithFile('data.txt')
+// fileGeneator.dealingWithFile(1)
 
