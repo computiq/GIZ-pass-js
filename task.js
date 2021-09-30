8e200c4f423af259b577d3b65721364628b17ca5
DevNoora <noora3lihussien99dev@gmail.com>
2021-09-30 04:27:15 +0300

    Solve Js Task

--- task.js
+++ task.js
@@ -8,33 +8,67 @@ let numbers = [
     15,
     16
 ];
-
+//Noora
 console.log("Original numbers list: ", numbers)
 
 for(let i = 0 ; i < numbers.length ; i++)
 {
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
+    for(let j = i ; j < numbers.length ; j++)
     {
-        if(numbers[i] > numbers[j + 1])
+        if(numbers[j] < numbers[i])
         {
             let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
+            numbers[j] = numbers[i]
+            numbers[i] = temp;
         }
     }
 }
 
 console.log("Numbers list After sorting: ", numbers)
 
+//OR Some change of laith function
+
+// for(let i = 0 ; i < numbers.length ; i++)
+// {
+//     for(let j = 0 ; j > numbers.length ; j++)
+//     {
+//         if(numbers[i] > numbers[j + 1])
+//         {
+//             let temp = numbers[j];
+//             numbers[j] = numbers[j + 1]
+//             numbers[j + 1] = temp;
+//         }
+//     }
+// }
+//console.log("Numbers list After sorting: ", numbers)
+
+//Laith
+// for(let i = 0 ; i < numbers.length ; i++)
+// {
+//     for(let j = 0 ; j < numbers.length - 1 ; j++)
+//     {
+//         if(numbers[i] > numbers[j + 1])
+//         {
+//             let temp = numbers[j];
+//             numbers[j] = numbers[j + 1]
+//             numbers[j + 1] = temp;
+//         }
+//     }
+// }
+
+// console.log("Numbers list After sorting: ", numbers)
+
+//Noora
+
 for(let i = 0 ; i < numbers.length ; i++)
 {
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
+    for(let j = i ; j < numbers.length ; j++)
     {
-        if(numbers[i] < numbers[j + 1])
+        if(numbers[j] > numbers[i])
         {
             let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
+            numbers[j] = numbers[i]
+            numbers[i] = temp;
         }
     }
 }
@@ -42,3 +76,19 @@ for(let i = 0 ; i < numbers.length ; i++)
 console.log("Numbers list After Desc sorting: ", numbers)
 
 
+//Laith
+
+// for(let i = 0 ; i < numbers.length ; i++)
+// {
+//     for(let j = 0 ; j < numbers.length - 1 ; j++)
+//     {
+//         if(numbers[i] < numbers[j + 1])
+//         {
+//             let temp = numbers[j];
+//             numbers[j] = numbers[j + 1]
+//             numbers[j + 1] = temp;
+//         }
+//     }
+// }
+
+// console.log("Numbers list After Desc sorting: ", numbers)
