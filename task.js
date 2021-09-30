943650cebc5d2e555ebd3523385e91d61318e047
mustafa bahedh <79375993+movy18x@users.noreply.github.com>
2021-09-30 22:34:53 +0300

    Add files via upload
    the solution the task.
    by mustafa bahedh

--- task.js
+++ task.js
@@ -1,44 +1,46 @@
-let numbers = [
-    5,
-    8,
-    0,
-    1,
-    9,
-    11,
-    15,
-    16
-];
+const numbers = [5, 8, 0, 1, 9, 11, 15, 16];
 
-console.log("Original numbers list: ", numbers)
+let mutableArr = numbers; 
 
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] > numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
+console.log("Original numbers list: ", mutableArr);
+
+//This function recieve an array of numbers and return the result by asc order 
+function asc(nums){
+    for (let i = 0; i < nums.length; i++) {
+		for (let j = 0; j < nums.length; j++) {
+			if (nums[j] > nums[j + 1]) {
+				let temp = nums[j];
+				nums[j] = nums[j + 1];
+				nums[j + 1] = temp;
+			}
+		}
+	}
+    return nums;
 }
 
-console.log("Numbers list After sorting: ", numbers)
-
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] < numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
+//This function recieve an array of numbers and return the result by desc order 
+function desc(nums){
+    for (let i = 0; i < nums.length; i++) {
+		for (let j = 0; j < nums.length; j++) {
+			if (nums[j] < nums[j + 1]) {
+				let temp = nums[j];
+				nums[j] = nums[j + 1];
+				nums[j + 1] = temp;
+			}
+		}
+	}
+    return nums;
 }
 
-console.log("Numbers list After Desc sorting: ", numbers)
+console.log("--------------\nThe numbers after Asc sorting: ", asc(mutableArr)); //Printing the result after applying asc order
+
+console.log("--------------\nThe numbers after Desc sorting: ", desc(mutableArr)); //Printing the result after applying desc order
+
+
+let fs = require('fs'); 
+let data = fs.readFileSync('data.txt', 'utf8'); // Reading the file data.txt
+var arrayNum = JSON.parse("["+data+"]"); // Parsing the text into array of numbers
+fs.writeFileSync("output.txt", "After aplying asc sorting: [ "+ asc(arrayNum) +" ]"); //Writing the result of asc order on new file call output.txt
+
 
 
